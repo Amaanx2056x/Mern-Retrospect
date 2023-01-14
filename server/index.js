14803654dee77f18726bde6a595033cf66c60e09
Amaanx2056x <weirdghost123@gmail.com>
2023-01-15 02:19:02 +0530

    dev fix

--- server/index.js
+++ server/index.js
@@ -9,4 +9,11 @@ express_basic_setup(app);
 app.use("/", require("./route/auth"));
 app.use("/note", require("./route/note"));
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//   });
+// }
+
 app.listen(port, () => console.log(`App is running on port ${port}`));
